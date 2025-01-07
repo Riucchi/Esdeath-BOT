e0cd4c336014f065c4be25119fbcf921d339ba6d
Lucas <lucasbecca3@hotmail.com>
2025-01-07 17:12:47 -0300

    edit de los comentarios

--- js/lenguage.js
+++ js/lenguage.js
@@ -1,16 +1,15 @@
-// Espera a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function() {
-    // Función para cambiar el idioma
+    
     function changeFooterLanguage(language) {
-        console.log("Selected language:", language); // Imprime el idioma seleccionado en la consola
+        console.log("Selected language:", language);
         if (language === 'en') {
-            window.location.href = './index_en.html'; // Redirige a la versión en inglés
+            window.location.href = './index_en.html'; 
         } else if (language === 'es') {
-            window.location.href = './index.html'; // Redirige a la versión en español
+            window.location.href = './index.html';
         }
     }
 
-    // Asigna el evento click a los elementos del dropdown
+    
     const dropdownItems = document.querySelectorAll('.dropdown-item');
     dropdownItems.forEach(item => {
         item.addEventListener('click', function() {
